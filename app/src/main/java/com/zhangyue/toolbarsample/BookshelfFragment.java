2959fb11360ddc949e92733e08cbc396c228d1ac
newportchen <milkappleg@aliyun.com>
2016-04-07 16:51:29 +0800

    init

--- app/src/main/java/com/zhangyue/toolbarsample/BookshelfFragment.java
+++ app/src/main/java/com/zhangyue/toolbarsample/BookshelfFragment.java
@@ -11,6 +11,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
+ * 类文件头注释
  * Created by sanwe_000 on 2016/2/16.
  */
 public class BookshelfFragment extends Fragment {
